7f5564fbb7beebb1f5c8f23b9117adca2e42de5c
KodeAkuma <akumakode@gmail.com>
2024-07-02 12:59:37 +0530

    Initial Commit

--- serve.js
+++ serve.js
@@ -1,16 +1,19 @@
-import { getPortPromise } from "portfinder";
-import { Server } from "node-static";
-import { createServer } from "http";
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const portfinder = require("portfinder");
+const { Server } = require("node-static");
+const http = require("http");
 
 /** @type {NWJS_Helpers.WindowOpenOption} */
 const options = {};
 
 const main = async () => {
-  const port = await getPortPromise({ port: 9000 });
+  const port = await portfinder.getPortPromise({ port: 9000 });
 
   var file = new Server("./app", { cache: 0 });
 
-  createServer((req, res) => {
+  http
+    .createServer((req, res) => {
       req
         .addListener("end", () =>
           file.serve(req, res, (err) => err && err.status === 404 && file.serveFile("/index.html", 200, {}, req, res)),
@@ -19,10 +22,8 @@ const main = async () => {
     })
     .listen(port, "localhost");
 
-  // eslint-disable-next-line no-undef
   nw.Window.open("http://localhost:" + port, options, () => {});
 };
 
-// eslint-disable-next-line no-undef
 if (process.env.NODE_ENV === "development") nw.Window.open("http://localhost:5173", options, () => {});
 else main();
